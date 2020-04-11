040ed2a39660de8329e43d2914b4a7c8eb108c8b
evokidzz <adikolo13@gmail.com>
2020-04-11 17:14:56 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -6,6 +6,7 @@ public class InputOutput{
         String nama;
         System.out.print("Masukkan Nama Anda: ");
         nama = scanner.nextLine();
+        System.out.println("Hai, "+nama);
         
     }
 }
